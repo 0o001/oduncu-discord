92e47b463a0fd6d0261199da38835566be2a9ab4
Mustafa Uzun <mustafauzun0@gmail.com>
2019-03-19 19:12:06 +0300

    Initial commit

---
+++ discord.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js'),
+      client = new Discord.Client();
+
+const settings = require('./settings.json');
+
+const answer = [
+    'Oduncu',
+    'Allah Allah',
+    'Yaparım',
+    'Efendim',
+    'Doğru',
+    'Emrin',
+    'Evet',
+    'Hazır',
+    'Tamam'
+]
+
+client.on('ready', () => {
+
+  console.log(`Oduncu Sunucuya Giriş Yaptı!`);
+
+});
+
+client.on('message', (msg) => {
+
+    msg.content = msg.content.trim();
+
+    if(msg.content === settings.prefix || msg.content.startsWith(settings.prefix + ' ')) {
+
+        if (msg.content) {
+
+            msg.reply(answer[Math.floor(Math.random() * answer.length)]);
+
+        }
+
+    }
+
+});
+
+client.login(settings.token);
